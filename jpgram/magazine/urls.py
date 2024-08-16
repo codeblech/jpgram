e22df304279160c31f4d6ed6719810d81860dcc7
codeblech <my.malikyash@gmail.com>
2024-08-16 22:29:54 +0530

    add files

---
+++ jpgram/magazine/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("cice_jiit/", views.cice_jiit, name="cice_jiit"),
+    path("crescendojiit/", views.crescendojiit, name="crescendojiit"),
+    path("gdscjiit/", views.gdscjiit, name="gdscjiit"),
+    path("jaypee_photo_enthusiasts_guild/", views.jaypee_photo_enthusiasts_guild, name="jaypee_photo_enthusiasts_guild"),
+    path("jhankaarjiit/", views.jhankaarjiit, name="jhankaarjiit"),
+    path("jiit_impressions/", views.jiit_impressions, name="jiit_impressions"),
+    path("jiityouthclub/", views.jiityouthclub, name="jiityouthclub"),
+    path("knuth_jiit/", views.knuth_jiit, name="knuth_jiit"),
+    path("nssjiit62/", views.nssjiit62, name="nssjiit62"),
+    path("osdcjiit/", views.osdcjiit, name="osdcjiit"),
+    path("parola_literaryhub/", views.parola_literaryhub, name="parola_literaryhub"),
+    path("radiance_hub/", views.radiance_hub, name="radiance_hub"),
+    path("thejaypeedebsoc/", views.thejaypeedebsoc, name="thejaypeedebsoc"),
+    path("thepageturnersociety/", views.thepageturnersociety, name="thepageturnersociety"),
+    path("thethespiancircle/", views.thethespiancircle, name="thethespiancircle"),
+    path("ucrjiit/", views.ucrjiit, name="ucrjiit"),
+]
