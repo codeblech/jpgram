9ce5d9bc48636d5c6a3e99c13ea34c7dfbedd1bd
codeblech <my.malikyash@gmail.com>
2024-08-26 11:03:12 +0530

    redirect / to /magazine

--- jpgram/jpgram/urls.py
+++ jpgram/jpgram/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("magazine/", include("magazine.urls")),
+    path("", RedirectView.as_view(url="magazine/", permanent=True)),
 ]
